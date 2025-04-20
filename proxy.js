9b0afb00bca55375c9f2f36920618cd18a24c347
IAMDBCOOPERR <reddyindra53@gmail.com>
2025-04-20 08:16:57 +0530

    sds

--- proxy.js
+++ proxy.js
@@ -1,38 +1,26 @@
-
 import express from 'express';
-import Redis from "ioredis";  
+import Redis from 'ioredis';
 
 const app = express();
 const PORT = 3000;
 
-
-
 // Create Redis client with credentials
 const redis = new Redis({
-    host: "redis-10235.c273.us-east-1-2.ec2.redns.redis-cloud.com" || 'localhost',
-    port: 10235 || 6379,
-    password:'tTPRnmAItp6h51LZwAfGpQEl3IzrKKjc',
-  });
+  host: 'redis-10235.c273.us-east-1-2.ec2.redns.redis-cloud.com',
+  port: 10235,
+  password: 'tTPRnmAItp6h51LZwAfGpQEl3IzrKKjc',
+});
 
 // Handle Redis errors
 redis.on('error', (err) => console.error('Redis error:', err));
+redis.on('connect', () => console.log('Connected to Redis'));
 
-// Connect to Redis
-(async () => {
-  try {
-    await redis.connect();
-    console.log('Connected to Redis');
-  } catch (err) {
-    console.error('Could not connect to Redis:', err);
-  }
-})();
-
-// Endpoint to fetch latest record
+// Endpoint to fetch latest records
 app.get('/latest', async (req, res) => {
   try {
     const results = await redis.zrevrange('recent_downloads', 0, 4);
 
-    console.log('Latest record:', results  );
+    console.log('Latest record:', results);
     if (results.length > 0) {
       res.json({ results });
     } else {
